b86e7dbc702353fb3952605a50bc23c15a0d4b1f
MyUsername <rosanio.c@gmail.com>
2024-10-17 14:53:36 +0200

    riscrittura secondo PSEUDO CODICE

--- js/02-dadi/script.js
+++ js/02-dadi/script.js
@@ -5,24 +5,38 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
-
+// Genero un numero a caso tra 1 e 6 e lo assegno al Computer
 const computerNumber = Math.floor(Math.random() * 6) + 1;
 console.log('Numero del Computer: ' + computerNumber)
 
+// Chiedo un numero al giocatore tramite Prompt
 let userNumber = parseInt(prompt('Inserisci un numero intero da 1 a 6'));
 
+// Dichiaro le condizioni in cui il umero inserito dal giocatore è valido
 let isValid_userNumber = (!isNaN(userNumber) && Number.isInteger(userNumber) && userNumber > 0 && userNumber <= 6);
-console.log(userNumber)
 
-if(!isValid_userNumber) {
 
-    while (!isValid_userNumber) {
-      userNumber = parseInt(prompt('Inserisci un numero intero da 1 a 6'));
-      console.log(userNumber)
-    }
 
-  } else if (userNumber > computerNumber) {
-    console.log('Hai vinto!')
-  } else {
-    console.log('Hai perso')
+// SE il numero inserito dal giocatore NON è valido,
+if (!isValid_userNumber) {
+
+  // FINCHE' il numero non è valido chiedo un nuovo numero al giocatore
+  while (!isValid_userNumber) {
+      userNumber = parseInt(prompt('Inserisci un numero intero da 1 a 6'))
   }
+
+  // ALTRIMENTI
+} else {
+
+// SE il numero del giocatore supera quello del Computer
+if (userNumber > computerNumber) {
+  // STAMPO il messaggio di vittoria
+  console.log('Hai vinto!')
+
+// ALTRIMENTI
+} else {
+  // STAMPO il messaggio di sconfitta
+  console.log('Hai perso')
+}
+
+}
