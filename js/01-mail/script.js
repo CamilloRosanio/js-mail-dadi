e6c0d7ace0f3dd0ea26a4054dbc0e6ad50a54ce9
MyUsername <rosanio.c@gmail.com>
2024-10-17 23:06:56 +0200

    EMAIL - Test soluzione 2

--- js/01-mail/script.js
+++ js/01-mail/script.js
@@ -5,20 +5,26 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull'esito del controllo.
 */
 
+
 // WHILE: Finchè X non è = Y richiedi X
 
-let valueX = prompt("Please enter X");
+// let valueX = prompt("Please enter X");
 
-while(valueX !== "xxx"){
-       valueX = prompt("Please enter X again"); 
-       console.log('Input: ' + valueX);
-}
+// while(valueX !== "xxx"){
+//        valueX = prompt("Please enter X again"); 
+//        console.log('Input: ' + valueX);
+// }
 
-console.log('valueX OK')
+// console.log('valueX OK')
 
 
-// // Ho una lista di Email valide
-// const emailList = ['1@test.it', '2@test.it', '3@test.it', '4@test.it']
+
+
+
+// SOLUZIONE #1 -----------------------------------------------------------------
+
+// // Ho un array di Email valide
+// const emailList = ['aaa', 'bbb', 'ccc', 'ddd']
 
 // // Dichiaro la variabile "i"
 // let i = 0;
@@ -27,7 +33,7 @@ console.log('valueX OK')
 // let userEmail = prompt("Please enter your Email.");
 // console.log('User input: ' + userEmail);
 
-// // FINCHE' l'utente non inserisce una Email valida
+// // FINCHE' l'utente non inserisce una Email inclusa nella lista
 // while (!emailList.includes(userEmail)) { 
 
 //     // Chiedo nuovamente la sua Email
@@ -39,3 +45,30 @@ console.log('valueX OK')
 // // Stampo un alert che conferma che l'Email inserita è in lista, quindi valida
 // alert("Email OK");
 // console.log('Email OK: ' + userEmail)
+
+
+
+
+
+// SOLUZIONE #2 -----------------------------------------------------------------
+
+// Ho un array di Email valide
+const emailList = ['aaa', 'bbb', 'ccc', 'ddd']
+
+// Chiedo all'utente la sua Email
+const userEmail = prompt("Please enter your Email.");
+
+// Dichiaro un array di utilità che conterrà l'indice della email cercata
+let searchedEmailIndex = [];
+
+for (let i = 0; i < emailList.length; i++) {
+
+    if (userEmail === emailList[i]) {
+      searchedEmailIndex.push(i);
+    } else {
+      console.log('Non corrisponde: ' + i)
+    }
+
+}
+
+console.log('Email valida: ' + emailList[searchedEmailIndex])
--- js/02-dadi/script.js
+++ js/02-dadi/script.js
@@ -10,20 +10,55 @@ const computerNumber = Math.floor(Math.random() * 6) + 1;
 console.log('Numero del Computer: ' + computerNumber)
 
 // Chiedo un numero al giocatore tramite Prompt
-const userNumber = parseInt(prompt('Inserisci un numero intero da 1 a 6'));
+let userNumber = parseInt(prompt('Inserisci un numero intero da 1 a 6'));
 console.log('Numero del Giocatore: ' + userNumber);
 
 // Dichiaro le condizioni in cui il umero inserito dal giocatore è valido
 let isValid_userNumber = (!isNaN(userNumber) && Number.isInteger(userNumber) && userNumber > 0 && userNumber <= 6);
 console.log('Il numero inserito è valido? ' + isValid_userNumber)
 
-let i = 0;
+// Dichiaro la variabile
+let outputMessage;
 
-// FINCHE' il numero non è valido chiedo un nuovo numero al giocatore
-while (userNumber !== isValid_userNumber) {
-  userNumber = parseInt(prompt('Inserisci un numero intero da 1 a 6'));
-  console.log('Numero non valido: ' + userNumber)
+// // SE il valore inserito NON è valido
+// if (!isValid_userNumber) {
+
+//     console.log('Numero non valido')
+
+// } else {
+
+//     outputMessage = computerNumber > userNumber ? 'Hai perso': 'Hai vinto!';
+//     console.log(outputMessage);
+
+// }
+
+
+// SE il valore inserito NON è valido
+while (!isValid_userNumber) {
+
+  console.log('Numero non valido');
+  userNumber = prompt('Inserisci nuovamente un numero valido da 1 a 6')
+
 }
+
+console.log('Numero valido')
+
+
+
+  // outputMessage = computerNumber > userNumber ? 'Hai perso': 'Hai vinto!';
+  // console.log(outputMessage);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // // SE il numero del giocatore supera quello del Computer
