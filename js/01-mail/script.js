74646edff45eac3fad868c4c3c7e2bd202985367
MyUsername <rosanio.c@gmail.com>
2024-10-17 14:43:28 +0200

    impostazione esercizi

--- js/01-mail/script.js
+++ js/01-mail/script.js
@@ -5,3 +5,31 @@ controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull'esito del controllo.
 */
 
+const emailList = ['laprima@email.it', 'laseconda@email.it', 'laterza@email.it']
+
+
+const userEmail = prompt('Inserisci la tua Email')
+
+
+for (let i = 0; i < emailList.length; i++) {
+    isValid_userEmail = (userEmail == emailList[i])
+    
+    console.log(emailList[i])
+
+    if (!isValid_userEmail) {
+      outputMessage = 'Email non trovata'
+    } else {
+      outputMessage = 'Email valida'
+    }
+
+    console.log(outputMessage)
+}
+
+
+// if (emailList.includes(userEmail)) {
+//   messaggio = 'Email valida'
+// } else {
+//   messaggio = 'Email NON valida'
+// }
+
+// console.log(messaggio)
--- js/02-dadi/script.js
+++ js/02-dadi/script.js
@@ -5,3 +5,24 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
+
+const computerNumber = Math.floor(Math.random() * 6) + 1;
+console.log('Numero del Computer: ' + computerNumber)
+
+let userNumber = parseInt(prompt('Inserisci un numero intero da 1 a 6'));
+
+let isValid_userNumber = (!isNaN(userNumber) && Number.isInteger(userNumber) && userNumber > 0 && userNumber <= 6);
+console.log(userNumber)
+
+if(!isValid_userNumber) {
+
+    while (!isValid_userNumber) {
+      userNumber = parseInt(prompt('Inserisci un numero intero da 1 a 6'));
+      console.log(userNumber)
+    }
+
+  } else if (userNumber > computerNumber) {
+    console.log('Hai vinto!')
+  } else {
+    console.log('Hai perso')
+  }
