48151df5a21759f5e00422a0637eb944de88a054
MyUsername <rosanio.c@gmail.com>
2024-10-18 13:16:10 +0200

    CORREZIONE esercizio Dadi con ciclo infinito di controllo sull'input dell'Utente tramite WHILE

--- js/02-dadi/script.js
+++ js/02-dadi/script.js
@@ -6,15 +6,34 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
 // Genero un numero a caso tra 1 e 6 e lo assegno al Computer
-const computerNumber = Math.floor(Math.random() * 6) + 1;
-console.log('Numero del Computer: ' + computerNumber)
+// In questi casi si fa: 
+// const computerNumber = Math.floor(Math.random() * (max - min + 1))) + min;
+
+const computerNumber = Math.floor(Math.random() * (6 -1 + 1)) + 1;
+console.log('Numero del Computer: ' + computerNumber);
 
 // Chiedo un numero al giocatore tramite Prompt
 let userNumber = parseInt(prompt('Inserisci un numero intero da 1 a 6'));
+
+/* Non posso dichiarare la condizione di validità con una variabile, altrimenti il mio WHILE considererà sempre il valore
+che esce per primo, che se è TRUE mi chiude il ciclo, ma se è FALSE il ciclo si ripete all'infinito. */
+// let isValid_userNumber = (!isNaN(userNumber) && Number.isInteger(userNumber) && userNumber > 0 && userNumber <= 6);
+
+// FINCHE' il numero inserito non è valido
+
+// Inserisco quindi le condizioni di validazione nel WHILE, così che siano analizzate ogni volta, e non un valore statico
+
+while (isNaN(userNumber) || userNumber > 6 || userNumber <= 0) {
+  userNumber = parseInt(prompt('Riprova inserendo un numero intero da 1 a 6'));
+  console.log('Valore non valido: ' + userNumber);
+}
+
 console.log('Numero del Giocatore: ' + userNumber);
 
-// Dichiaro le condizioni in cui il umero inserito dal giocatore è valido
-let isValid_userNumber = (!isNaN(userNumber) && Number.isInteger(userNumber) && userNumber > 0 && userNumber <= 6);
-console.log('Il numero inserito è valido? ' + isValid_userNumber)
-
-
+if (userNumber > computerNumber) {
+  console.log('Hai vinto');
+} else if (userNumber < computerNumber) {
+  console.log('Hai perso');
+} else {
+  console.log('Pareggio');
+}
